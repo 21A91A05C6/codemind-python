72a6721ef5625828630cb3e2381da7fb1c796baf
Manju-Komarapuri <97424965+21A91A05C6@users.noreply.github.com>
2022-07-10 11:15:18 +0530

    n=int(input()) arr=list(map(int,input().split())) k=[] c=0 for i in arr:     if i==arr.count(i):         #print(i)         c+=1         k.append(i) print(len(set(k)))

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -1,8 +1,10 @@
 n=int(input())
-b=[]
-a=list(map(int,input().split()))
-for i in a:
-    if i==a.count(i):
-        b.append(i)
-b=set(b)
-print(len(b))
+arr=list(map(int,input().split()))
+k=[]
+c=0
+for i in arr:
+    if i==arr.count(i):
+        #print(i)
+        c+=1
+        k.append(i)
+print(len(set(k)))
