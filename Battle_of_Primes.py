6b090251a916af2e2a25822f846bebd506e2ea9c
Manju-Komarapuri <97424965+21A91A05C6@users.noreply.github.com>
2022-07-21 11:14:50 +0530

    def prime(s):     for i in range(1,10):         s1=s+i         for j in range(2,s1):             if(s1%j==0):                 break         else:             k=i             break     return k a=int(input()) b=int(input()) s=a+b print(prime(s))

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,12 +1,10 @@
 def prime(s):
-    c=0
     for i in range(1,10):
         s1=s+i
-        c=0
         for j in range(2,s1):
             if(s1%j==0):
-                c+=1
-        if(c==0):
+                break
+        else:
             k=i
             break
     return k
