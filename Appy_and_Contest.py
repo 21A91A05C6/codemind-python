1209137caef85c9a618648ef0d7db216ceb9d0dc
Manju-Komarapuri <97424965+21A91A05C6@users.noreply.github.com>
2022-10-02 18:17:13 +0530

    t=int(input()) for i in range(t):     n,a,b,k=map(int,input().split())     c=0     if(a%b==0):         c=n/b-n/a     elif(b%a==0):         c=n/a-n/b     else:         c=n/a+n/b-2*(n/(a*b))     if(c>=k):         print("Win")     else:         print("Lose")

---
+++ Appy_and_Contest.py
@@ -0,0 +1,14 @@
+t=int(input())
+for i in range(t):
+    n,a,b,k=map(int,input().split())
+    c=0
+    if(a%b==0):
+        c=n/b-n/a
+    elif(b%a==0):
+        c=n/a-n/b
+    else:
+        c=n/a+n/b-2*(n/(a*b))
+    if(c>=k):
+        print("Win")
+    else:
+        print("Lose")
