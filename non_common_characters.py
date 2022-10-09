572f48e927ba3aab023bc1f88385a8c8615ad07f
Manju-Komarapuri <97424965+21A91A05C6@users.noreply.github.com>
2022-10-09 10:54:21 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() k=[] l=[] c=0 cc=0 m="" for i in s2:     if i not in s1:         k.append(i) for i in s1:     if i not in s2:         l.append(i) k=set(k) l=set(l) for i in k:     if i not in " ":         m+=i for i in l:     if i not in " ":
            m+=i
    m=sorted(m)
    for i in m:
        print(i,end="")

--- non_common_characters.py
+++ non_common_characters.py
@@ -2,16 +2,27 @@ s1=input()
 s2=input()
 s1=s1.lower()
 s2=s2.lower()
-k=""
-l=""
+k=[]
+l=[]
+c=0
+cc=0
 m=""
-n=""
-o=""
-for i in s1:
-    if i not in s2 and i!=" ":
-        k+=i
 for i in s2:
-    if i not in s1 and i!=" ":
-        l+=i
-m=sorted(set(k+l))
-print("".join(m))
+    if i not in s1:
+        k.append(i)
+for i in s1:
+    if i not in s2:
+        l.append(i)
+k=set(k)
+l=set(l)
+for i in k:
+    if i not in " ":
+        m+=i
+for i in l:
+    if i not in " ":
+        
+        m+=i
+m=sorted(m)
+for i in m:
+    print(i,end="")
+    
