04f21bc0b59fa32446774238bed993f8fd3d3fe7
Manju-Komarapuri <97424965+21A91A05C6@users.noreply.github.com>
2022-10-11 22:52:59 +0530

    n=int(input()) arr=list(map(int,input().split())) c=0 arr.append(arr[0]) arr.append(arr[1]) for i in range(0,len(arr)-2):     if arr[i]%2==0 and arr[i+2]%2!=0 or arr[i]%2!=0 and arr[i+2]%2==0:         c+=1 print(c)

--- count_elements_in_an_array_in_circular_form.py
+++ count_elements_in_an_array_in_circular_form.py
@@ -1,10 +1,9 @@
 n=int(input())
 arr=list(map(int,input().split()))
+c=0
 arr.append(arr[0])
 arr.append(arr[1])
-c=0
-#print(arr)
 for i in range(0,len(arr)-2):
-    if(arr[i]%2==0 and arr[i+2]%2!=0 or arr[i]%2!=0 and arr[i+2]%2==0):
+    if arr[i]%2==0 and arr[i+2]%2!=0 or arr[i]%2!=0 and arr[i+2]%2==0:
         c+=1
 print(c)
